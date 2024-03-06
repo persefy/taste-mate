8201ec74be0d7bae4c8bf57811f4ad2a4e98be79
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-03-05 20:21:00 -0500

    added code for capturing search query in search bar

--- taste-mate/src/components/SearchBar.jsx
+++ taste-mate/src/components/SearchBar.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react'
+import '../App.css'
+
 export default function SearchBar () {
+    const initialState = {
+        searchQuery: '',
+    }
+    const [formState, setFormState] = useState(initialState);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        //doing something with data
+
+        console.log(formState)
+        //reverting to our initial state
+
+        setFormState(initialState)
+
+    }
+
+    const handleChange = (event) => {
+        setFormState({...formState, [event.target.id]: event.target.value})
+    }
     return (
-        <form>
-            <input type="text"/>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="searchQuery"
+                        onChange={handleChange}
+                        value={formState.searchQuery}/>
+                <button type="submit">Search</button>
+            </form>
+        </div>
+
+
     )
 }
--- taste-mate/src/components/SearchResults.jsx
+++ taste-mate/src/components/SearchResults.jsx
@@ -1,3 +1,6 @@
+import SearchBar from "./SearchBar"
+import axios from "axios"
+
 export default function SearchResults () {
     return (
         <div></div>
